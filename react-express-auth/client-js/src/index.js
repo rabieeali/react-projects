5290945cada68bc2e2dfc5bf6919d9e69d3910e5
rabieeali <alirbi.webdev@gmail.com>
2023-07-16 16:04:32 +0330

    added js version of react-express auth

--- react-express-auth/client-js/src/index.js
+++ react-express-auth/client-js/src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import './styles/bootstrap.min.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/bootstrap.min.css'
+import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie';
 import AuthProvider from './context/AuthProvider';
