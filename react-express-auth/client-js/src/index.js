3b115e76ff8de6d55dc8c723593d54ebd1be373a
Ali Rabiee <92788225+rabieeali@users.noreply.github.com>
2023-07-16 16:02:55 +0330

    Update index.js

--- react-express-auth/client-js/src/index.js
+++ react-express-auth/client-js/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 import './styles/bootstrap.min.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
