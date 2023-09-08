eb1f9cf5f031cdc0c2c20fc64fa1513e7237c433
ali.rabiee <a.rabiee@fidilio.com>
2023-09-08 13:47:56 +0330

    added legacy mern graphql

--- legacy-graphql/client/components/SongCreate.js
+++ legacy-graphql/client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link, hashHistory } from 'react-router'
+import query from '../queries/fetchSongs'
+
 
 class SongCreate extends Component {
     constructor(props) {
@@ -12,22 +15,25 @@ class SongCreate extends Component {
     submitHandler(e) {
         e.preventDefault()
         this.props.mutate({
-            variables: {
-                title: this.state.title
-            }
+            variables: { title: this.state.title },
+            refetchQueries: [{ query }]
         })
+            .then(() => this.setState({ title: '' }))
+            .then(() => hashHistory.push('/'))
+            .catch((err) => console.log(err))
     }
 
     render() {
         return (
             <div>
+                <Link to='/'>{'<'} Back</Link>
                 <h3>Create a New Song!</h3>
                 <form onSubmit={this.submitHandler.bind(this)}>
                     <label htmlFor="song">Song Title</label>
                     <input
                         value={this.state.title}
                         onChange={e => this.setState({ title: e.target.value })} />
-                    <button>create</button>
+                    <button className='btn green'>create</button>
                 </form>
             </div>
         )
--- legacy-graphql/client/components/SongList.js
+++ legacy-graphql/client/components/SongList.js
@@ -1,31 +1,47 @@
 import React from "react"
-import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 import Spinner from "./Spinner"
+import { Link } from 'react-router'
+import query from '../queries/fetchSongs'
+import gql from 'graphql-tag'
 
 const SongList = (props) => {
-    const { songs, loading } = props.data
+    const { songs, loading, refetch } = props.data
+    const { mutate } = props
+
+    const deleteHandler = (id) => {
+        mutate({ variables: { id } })
+        .then(() => refetch())
+    }
 
     if (loading) return <Spinner />
     return (
         <div>
-            <ul className="collection">
-                {songs.map(song => (
-                    <li className="collection-items" key={song.id}>{song.title}</li>
-                ))}
-            </ul>
+            <div>
+                <ul className="collection">
+                    {songs.map(song => (
+                        <li style={{ display: 'flex', justifyContent: 'space-between' }} className="collection-item" key={song.id}>
+                            <span>{song.title}</span>
+                            <span onClick={() => deleteHandler(song.id)} style={{ cursor: 'pointer', color: 'red' }}><i className="material-icons">delete</i></span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <Link to='/songs/new' className='btn-floating btn-large red right'>
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         </div>
     )
 }
 
-// does not execute! just define the query
-const query = gql`
-{
-    songs {
-        id
-        title
-    }
-}
-`;
+const mutation = gql`
+        mutation DeleteSong($id:ID){
+            deleteSong(id:$id){
+                id
+            }
+        }
+`
 
-export default graphql(query)(SongList)
+export default graphql(mutation)(graphql(query)(SongList))
